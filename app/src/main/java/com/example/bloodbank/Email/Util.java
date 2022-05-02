cf85f6ed940af55f8828fa165d4f1a0d46be7888
nitishmwn44 <nitishmwn44@gmail.com>
2022-05-02 11:45:47 +0530

    initial commit

---
+++ app/src/main/java/com/example/bloodbank/Email/Util.java
@@ -0,0 +1,8 @@
+package com.example.bloodbank.Email;
+
+public class Util {
+    // Add your own email and password over here.
+    public  static  final String EMAIL = "ankitkumara429@gmail.com";
+    public static final String PASSWORD = "Bgp@123456";
+}
+
